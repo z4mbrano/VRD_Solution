b7efcd2908788f31a1d6d7e3a53f66a76e97b1b8
z4mbrano <guilherrme.bohn@gmail.com>
2024-08-08 15:00:48 -0300

    add animacoes e login

--- script/script.js
+++ script/script.js
@@ -3,3 +3,28 @@ function rolarParaSecao() {
     var secao = document.getElementById("secao-alvo");
     secao.scrollIntoView({ behavior: 'smooth' });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target); // Para observar apenas uma vez
+            }
+        });
+    });
+
+    const elementsToAnimate = document.querySelectorAll(".animate-on-scroll");
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+function toggleDiv() {
+    var login = document.getElementById("login");
+    if (login.style.display === "none" || login.style.display === "") {
+        login.style.display = "block";
+    } else {
+        login.style.display = "none";
+    }
+}
